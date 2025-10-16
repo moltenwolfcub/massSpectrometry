a5f6f5e51726d9505d6c7dae61cef5b8b58f0cf0
MoltenWolfCub <oliver@tomlins.org.uk>
2025-10-16 22:35:41 +0100

    created data model for molecules

---
+++ molecule.go
@@ -0,0 +1,30 @@
+package main
+
+var (
+	CARBON = Atom{
+		Name:         "carbon",
+		AtomicNumber: 6,
+		AtomicMass:   12.0,
+	}
+
+	HYDROGEN = Atom{
+		Name:         "hydrogen",
+		AtomicNumber: 1,
+		AtomicMass:   1.0,
+	}
+)
+
+type Atom struct {
+	Name         string
+	AtomicNumber int
+	AtomicMass   float32
+}
+
+type Molecule struct {
+	Name  string
+	Atoms []struct {
+		element *Atom
+		count   int
+	}
+	Charge int
+}
